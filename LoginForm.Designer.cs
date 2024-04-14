733be5b83eba45374ca701cd73b343169b6df1b6
AdeptOfKnowledge <166988810+AdeptOfKnowledge@users.noreply.github.com>
2024-04-14 21:56:10 +0300

    SignIn+Moving+Close

--- LoginForm.Designer.cs
+++ LoginForm.Designer.cs
@@ -30,11 +30,12 @@
         {
             this.AuthorizationText = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.Password = new System.Windows.Forms.TextBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.Login = new System.Windows.Forms.TextBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.Exit = new System.Windows.Forms.Button();
+            this.Password = new System.Windows.Forms.TextBox();
+            this.Login = new System.Windows.Forms.TextBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.SignIn = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
@@ -53,6 +54,8 @@
             this.AuthorizationText.TabIndex = 0;
             this.AuthorizationText.Text = "Authorization";
             this.AuthorizationText.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.AuthorizationText.MouseDown += new System.Windows.Forms.MouseEventHandler(this.AuthorizationText_MouseDown);
+            this.AuthorizationText.MouseMove += new System.Windows.Forms.MouseEventHandler(this.AuthorizationText_MouseMove);
             // 
             // panel1
             // 
@@ -63,43 +66,6 @@
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(334, 100);
             this.panel1.TabIndex = 1;
-            // 
-            // Password
-            // 
-            this.Password.Font = new System.Drawing.Font("Times New Roman", 16.25F);
-            this.Password.Location = new System.Drawing.Point(82, 183);
-            this.Password.Name = "Password";
-            this.Password.Size = new System.Drawing.Size(210, 32);
-            this.Password.TabIndex = 8;
-            this.Password.UseSystemPasswordChar = true;
-            // 
-            // pictureBox2
-            // 
-            this.pictureBox2.Image = global::Authorization.Properties.Resources._3d_lock;
-            this.pictureBox2.Location = new System.Drawing.Point(42, 183);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(32, 32);
-            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox2.TabIndex = 7;
-            this.pictureBox2.TabStop = false;
-            // 
-            // Login
-            // 
-            this.Login.Font = new System.Drawing.Font("Times New Roman", 16.25F);
-            this.Login.Location = new System.Drawing.Point(82, 145);
-            this.Login.Name = "Login";
-            this.Login.Size = new System.Drawing.Size(210, 32);
-            this.Login.TabIndex = 6;
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = global::Authorization.Properties.Resources.programmer;
-            this.pictureBox1.Location = new System.Drawing.Point(42, 145);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(32, 32);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 5;
-            this.pictureBox1.TabStop = false;
             // 
             // Exit
             // 
@@ -120,6 +86,59 @@
             this.Exit.Text = "x";
             this.Exit.TextAlign = System.Drawing.ContentAlignment.TopRight;
             this.Exit.UseVisualStyleBackColor = false;
+            this.Exit.Click += new System.EventHandler(this.Exit_Click);
+            // 
+            // Password
+            // 
+            this.Password.Font = new System.Drawing.Font("Times New Roman", 16.25F);
+            this.Password.Location = new System.Drawing.Point(80, 180);
+            this.Password.Name = "Password";
+            this.Password.Size = new System.Drawing.Size(215, 32);
+            this.Password.TabIndex = 8;
+            this.Password.UseSystemPasswordChar = true;
+            // 
+            // Login
+            // 
+            this.Login.Font = new System.Drawing.Font("Times New Roman", 16.25F);
+            this.Login.Location = new System.Drawing.Point(80, 140);
+            this.Login.Name = "Login";
+            this.Login.Size = new System.Drawing.Size(215, 32);
+            this.Login.TabIndex = 6;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Authorization.Properties.Resources._3d_lock;
+            this.pictureBox2.Location = new System.Drawing.Point(40, 180);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(32, 32);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Authorization.Properties.Resources.programmer;
+            this.pictureBox1.Location = new System.Drawing.Point(40, 140);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(32, 32);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // SignIn
+            // 
+            this.SignIn.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.SignIn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.SignIn.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveCaption;
+            this.SignIn.FlatAppearance.BorderSize = 0;
+            this.SignIn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.SignIn.Font = new System.Drawing.Font("Times New Roman", 14.75F);
+            this.SignIn.Location = new System.Drawing.Point(80, 280);
+            this.SignIn.Name = "SignIn";
+            this.SignIn.Size = new System.Drawing.Size(75, 32);
+            this.SignIn.TabIndex = 6;
+            this.SignIn.Text = "Sign in";
+            this.SignIn.UseVisualStyleBackColor = false;
             // 
             // LoginForm
             // 
@@ -127,6 +146,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.InactiveCaption;
             this.ClientSize = new System.Drawing.Size(334, 361);
+            this.Controls.Add(this.SignIn);
             this.Controls.Add(this.Password);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.Login);
@@ -153,6 +173,7 @@
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.TextBox Login;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button SignIn;
     }
 }
 
---
+++ LoginForm.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Authorization
+{
+    public partial class LoginForm : Form
+    {
+        Point NP;
+
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void AuthorizationText_MouseDown(object sender, MouseEventArgs e)
+        {
+            NP = new Point(e.X, e.Y);
+        }
+
+        private void AuthorizationText_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Left += e.X - NP.X;
+                this.Top += e.Y - NP.Y;
+            }         
+        }
+    }
+}
